463492f1004decfee3dfe4a90740ab63d21dc9f8
vojtechrichter <vojta3218@gmail.com>
2025-07-23 20:36:06 +0200

    feat: minor updates

--- src/NextRunResolver.php
+++ src/NextRunResolver.php
@@ -20,7 +20,9 @@ final readonly class NextRunResolver
         $next = $from->add(new \DateInterval('PT1S'));
         $validator = new Validator($this->fields);
 
-        for ($i = 0; $i < Parser::MAX_ITERATIONS; $i++) {
+        $maxIterations = 100;
+
+        for ($i = 0; $i < $maxIterations; $i++) {
             if ($validator->isValid($next)) {
                 return $next;
             }
@@ -28,7 +30,7 @@ final readonly class NextRunResolver
             $next = $this->getNextRunCandidate($next);
         }
 
-        throw new \RuntimeException('Could not find next execution time within 4 years');
+        throw new \RuntimeException('Could not find next execution time within reasonable iterations');
     }
 
     private function getNextRunCandidate(\DateTimeImmutable $datetime): \DateTimeImmutable
@@ -49,31 +51,79 @@ final readonly class NextRunResolver
         // Try to increment minute and reset seconds
         $nextMinute = $this->getNextValue($minute, $this->fields[FieldType::Minute->value]);
         if ($nextMinute > $minute) {
-            return $datetime->setTime($hour, $nextMinute, $this->fields[FieldType::Second->value][0]);
+            return $datetime->setTime($hour, $nextMinute, $this->getFirstValue($this->fields[FieldType::Second->value]));
         }
 
         // Try to increment hour and reset minutes & seconds
         $nextHour = $this->getNextValue($hour, $this->fields[FieldType::Hour->value]);
         if ($nextHour > $hour) {
-            return $datetime->setTime($nextHour, $this->fields[FieldType::Minute->value][0], $this->fields[FieldType::Second->value][0]);
+            return $datetime->setTime(
+                $nextHour,
+                $this->getFirstValue($this->fields[FieldType::Minute->value]),
+                $this->getFirstValue($this->fields[FieldType::Second->value])
+            );
         }
 
         // Try to increment day and reset time
         $nextDay = $this->getNextValue($day, $this->fields[FieldType::Day->value]);
         if ($nextDay > $day && $nextDay <= cal_days_in_month(CAL_GREGORIAN, $month, $year)) {
-            return $datetime->setDate($year, $month, $day)
-                ->setTime($this->fields[FieldType::Hour->value][0], $this->fields[FieldType::Minute->value][0], $this->fields[FieldType::Second->value][0]);
+            return $datetime->setDate($year, $month, $nextDay)
+                ->setTime(
+                    $this->getFirstValue($this->fields[FieldType::Hour->value]),
+                    $this->getFirstValue($this->fields[FieldType::Minute->value]),
+                    $this->getFirstValue($this->fields[FieldType::Second->value])
+                );
+        }
+
+        // Try to increment month and reset day & time
+        $nextMonth = $this->getNextValue($month, $this->fields[FieldType::Month->value]);
+        if ($nextMonth > $month) {
+            $validDay = $this->getValidDayForMonth($year, $nextMonth);
+            return $datetime->setDate($year, $nextMonth, $validDay)
+                ->setTime(
+                    $this->getFirstValue($this->fields[FieldType::Hour->value]),
+                    $this->getFirstValue($this->fields[FieldType::Minute->value]),
+                    $this->getFirstValue($this->fields[FieldType::Second->value])
+                );
         }
 
         // Increment year and reset everything
-        $nextMonth = $this->getNextValue($month, $this->fields[FieldType::Month->value]);
-        if ($nextMonth > $month) {
-            return $datetime->setDate($year, $nextMonth, $this->fields[FieldType::Day->value][0])
-                ->setTime($this->fields[FieldType::Hour->value][0], $this->fields[FieldType::Minute->value][0], $this->fields[FieldType::Second->value][0]);
+        $nextYear = $year + 1;
+        $firstMonth = $this->getFirstValue($this->fields[FieldType::Month->value]);
+        $validDay = $this->getValidDayForMonth($nextYear, $firstMonth);
+
+        return $datetime->setDate($nextYear, $firstMonth, $validDay)
+            ->setTime(
+                $this->getFirstValue($this->fields[FieldType::Hour->value]),
+                $this->getFirstValue($this->fields[FieldType::Minute->value]),
+                $this->getFirstValue($this->fields[FieldType::Second->value])
+            );
+    }
+
+    private function getValidDayForMonth(int $year, int $month): int
+    {
+        $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+        $validDays = $this->fields[FieldType::Day->value];
+
+        sort($validDays);
+
+        foreach ($validDays as $day) {
+            if ($day <= $daysInMonth) {
+                return $day;
+            }
         }
 
-        return $datetime->setDate($year + 1, $this->fields[FieldType::Month->value][0], $this->fields[FieldType::Day->value][0])
-            ->setTime($this->fields[FieldType::Hour->value][0], $this->fields[FieldType::Minute->value][0], $this->fields[FieldType::Second->value][0]);
+        return $validDays[0];
+    }
+
+    /**
+     * Get the first (smallest) valid value from an array
+     */
+    private function getFirstValue(array $validValues): int
+    {
+        $sorted = $validValues;
+        sort($sorted);
+        return $sorted[0];
     }
 
     /**
@@ -91,6 +141,6 @@ final readonly class NextRunResolver
             }
         }
 
-        return $validValues[0];
+        return $current; // Return current if no next value found (signals need to increment higher field)
     }
 }
